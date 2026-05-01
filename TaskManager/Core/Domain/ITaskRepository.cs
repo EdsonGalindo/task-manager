fa9b95bb81921d9aabef2ea77be8bc88f3cb64d9
Edson Galindo <edson@yanel.com.br>
2026-05-01 16:31:48 -0300

    Adiciona projeto da camada de dados do Core do sistema e a interface para implementação do repositório de Tarefas

---
+++ TaskManager/Core/Domain/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public interface ITaskRepository
+    {
+        Task GetTaskById(int id);
+        IEnumerable<Task> GetAllTasks(int? id, TaskStatusEnum.Status? status);
+        void AddTask(Task task);
+        void UpdateTask(Task task);
+        void DeleteTask(int id);
+    }
+}
