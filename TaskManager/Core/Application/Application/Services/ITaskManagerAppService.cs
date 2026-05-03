c5c758c4b032085dee3a90d8d81a3f6aed58752a
Edson Galindo <edson@yanel.com.br>
2026-05-03 11:34:59 -0300

    Adiciona interface para o serviço de aplicação do gerenciador de tarefas

---
+++ TaskManager/Core/Application/Application/Services/ITaskManagerAppService.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+
+namespace Application.Services
+{
+    public interface ITaskManagerAppService
+    {
+        Task<IEnumerable<TaskDto>> GetAllTasksAsync(DateTime? date, TaskStatusEnum.Status? taskStatus);
+        Task<TaskDto> GetTaskByIdAsync(int id);
+        Task<bool> CreateTaskAsync(TaskDto taskDto);
+        Task<bool> UpdateTaskAsync(TaskDto taskDto);
+        Task<bool> DeleteTaskAsync(int id);
+    }
+}
