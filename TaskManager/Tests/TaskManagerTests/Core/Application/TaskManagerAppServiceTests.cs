7294a38ca479fd08a95817fa679bb8f9b870ddc7
Edson Galindo <edson@yanel.com.br>
2026-05-03 15:03:49 -0300

    Adiciona teste para atualização de entidade de Tarefa inválida

--- TaskManager/Tests/TaskManagerTests/Core/Application/TaskManagerAppServiceTests.cs
+++ TaskManager/Tests/TaskManagerTests/Core/Application/TaskManagerAppServiceTests.cs
@@ -240,7 +240,7 @@ namespace TaskManagerTests.Core.Application
 
             var result = await _taskManagerAppService.GetAllTasksAsync(
                 taskDto1.DueDate, TaskStatusEnum.Status.Pending);
-            
+
             Assert.NotNull(result);
             Assert.Single(result);
             Assert.Equal(1, result.First().Id);
@@ -270,9 +270,32 @@ namespace TaskManagerTests.Core.Application
 
             var result = await _taskManagerAppService.GetAllTasksAsync(
                 DateTime.Now.AddDays(2), TaskStatusEnum.Status.Completed);
-            
+
             Assert.NotNull(result);
             Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task UpdateTaskAsync_WhenTaskDtoIsInvalid_ThrowsException()
+        {
+            var taskDto = new TaskDto()
+            {
+                Id = 1,
+                Title = TASK_TITLE,
+                Description = TASK_DESCRIPTION,
+                DueDate = DateTime.Now,
+                Status = TaskStatusEnum.Status.Pending
+            };
+            await _taskManagerAppService.CreateTaskAsync(taskDto);
+            _context.ChangeTracker.Clear();
+
+#pragma warning disable CS8625
+            taskDto.Title = null;
+#pragma warning restore CS8625
+            var result = await Assert.ThrowsAsync<Exception>(async () => await _taskManagerAppService.UpdateTaskAsync(taskDto));
+
+            Assert.NotNull(result);
+            Assert.Equal("Erro ao atualizar tarefa", result.Message);
         }
     }
 }
