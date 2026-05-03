e6836224a2f1428a568e883ba1a395b66355f8b6
Edson Galindo <edson@yanel.com.br>
2026-05-03 18:19:39 -0300

    Adiciona extensão para registro de dependências do Core do sistema na API

---
+++ TaskManager/WebApps/TaskManager.WebApp.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using Application.Services;
+using Data;
+using Data.Repositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.WebApp.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            services.AddScoped<ITaskManagerAppService, TaskManagerAppService>();
+            
+            services.AddDbContext<TaskContext>(options => 
+                options.UseSqlite("Filename=:memory:"));
+
+            return services;
+        }
+    }
+}
