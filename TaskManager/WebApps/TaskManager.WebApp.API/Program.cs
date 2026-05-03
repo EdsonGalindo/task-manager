27c8e0077a9179606511a42b946e98740ca67acb
Edson Galindo <edson@yanel.com.br>
2026-05-03 20:00:24 -0300

    Adiciona configuração do swagger e do banco de dados no startup da API

--- TaskManager/WebApps/TaskManager.WebApp.API/Program.cs
+++ TaskManager/WebApps/TaskManager.WebApp.API/Program.cs
@@ -1,3 +1,4 @@
+using Data;
 using TaskManager.WebApp.API.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerConfig();
 builder.Services.RegisterDependencies();
 
 var app = builder.Build();
@@ -18,6 +19,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+#region Garante que o banco foi criado ao iniciar a aplicação, utilizando o contexto de banco de dados para criar o banco caso ele ainda não exista.
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<TaskContext>();
+context.Database.EnsureCreated();
+#endregion
 
 app.UseHttpsRedirection();
 
