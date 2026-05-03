2d4af5c113ba0a1018b68d9cb15c908e9c543653
Edson Galindo <edson@yanel.com.br>
2026-05-03 13:13:13 -0300

    Adiciona configuração de colunas das entidades no banco de dados

---
+++ TaskManager/Core/Data/Configurations/TaskConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Task = Domain.Task;
+
+namespace Data.Configurations
+{
+    public class TaskConfiguration : IEntityTypeConfiguration<Task>
+    {
+        public void Configure(EntityTypeBuilder<Task> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Title)
+                    .IsRequired()
+                    .HasMaxLength(200);
+    
+            builder.Property(t => t.Description)
+                .HasMaxLength(1000);
+    
+            builder.Property(t => t.Status)
+                .IsRequired();
+        }
+    }
+}
