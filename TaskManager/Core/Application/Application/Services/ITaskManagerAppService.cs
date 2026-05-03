beac2f481f9fca40c952451e51e059eaad89b14d
Edson Galindo <edson@yanel.com.br>
2026-05-03 12:19:09 -0300

    Adiciona documentação da interface de serviço de aplicação do gerenciador de tarefas e seus métodos e na classe concreta

--- TaskManager/Core/Application/Application/Services/ITaskManagerAppService.cs
+++ TaskManager/Core/Application/Application/Services/ITaskManagerAppService.cs
@@ -3,12 +3,46 @@ using Domain;
 
 namespace Application.Services
 {
+    /// <summary>
+    /// Interface para o serviço de gerenciamento de tarefas, definindo os métodos para criar, ler, atualizar e excluir tarefas.
+    /// </summary>
     public interface ITaskManagerAppService
     {
+        /// <summary>
+        /// Obtém todas as tarefas, podendo ser filtradas por data de vencimento e/ou status. 
+        /// Se nenhum filtro for fornecido, retorna todas as tarefas.
+        /// </summary>
+        /// <param name="date">Data de vencimento para filtrar as tarefas.</param>
+        /// <param name="taskStatus">Status da tarefa para filtrar as tarefas.</param>
+        /// <returns>Uma coleção de TaskDto que atende aos critérios de filtro.</returns>
         Task<IEnumerable<TaskDto>> GetAllTasksAsync(DateTime? date, TaskStatusEnum.Status? taskStatus);
+
+        /// <summary>
+        /// Obtém uma tarefa específica pelo seu ID. Se a tarefa não for encontrada, lança uma exceção.
+        /// </summary>
+        /// <param name="id">ID da tarefa a ser obtida.</param>
+        /// <returns>O TaskDto correspondente ao ID fornecido.</returns>
         Task<TaskDto> GetTaskByIdAsync(int id);
+
+        /// <summary>
+        /// Cria uma nova tarefa com base no TaskDto fornecido. Se a criação for bem-sucedida, retorna o TaskDto criado; caso contrário, lança uma exceção.
+        /// </summary>
+        /// <param name="taskDto">Um objeto TaskDto contendo os dados da tarefa a ser criada.</param>
+        /// <returns>O TaskDto criado.</returns>
         Task<TaskDto> CreateTaskAsync(TaskDto taskDto);
+
+        /// <summary>
+        /// Atualiza uma tarefa existente com base no TaskDto fornecido. Retorna true se a atualização for bem-sucedida; caso contrário, retorna false.
+        /// </summary>
+        /// <param name="taskDto">Um objeto TaskDto contendo os dados da tarefa a ser atualizada.</param>
+        /// <returns>True se a atualização for bem-sucedida; caso contrário, false. </returns>
         Task<bool> UpdateTaskAsync(TaskDto taskDto);
+
+        /// <summary>
+        /// Remove uma tarefa específica pelo seu ID. Retorna true se a exclusão for bem-sucedida; caso contrário, retorna false.
+        /// </summary>
+        /// <param name="id">ID da tarefa a ser removida.</param>
+        /// <returns>True se a exclusão for bem-sucedida; caso contrário, false.</returns>
         Task<bool> DeleteTaskAsync(int id);
     }
 }
--- TaskManager/Core/Application/Application/Services/TaskManagerAppService.cs
+++ TaskManager/Core/Application/Application/Services/TaskManagerAppService.cs
@@ -4,10 +4,16 @@ using Task = Domain.Task;
 
 namespace Application.Services
 {
+    /// <summary>
+    /// Application service para gerenciamento de tarefas, implementando a interface ITaskManagerAppService e 
+    /// utilizando o repositório ITaskRepository para realizar operações de CRUD em tarefas.
+    /// </summary>
+    /// <param name="taskRepository">O repositório de tarefas a ser utilizado pelo serviço.</param>
     public class TaskManagerAppService(ITaskRepository taskRepository) : ITaskManagerAppService
     {
         private readonly ITaskRepository _taskRepository = taskRepository;
 
+        /// <inheritdoc/>
         public async Task<TaskDto> CreateTaskAsync(TaskDto taskDto)
         {
             return await _taskRepository.AddTaskAsync(taskDto.ToTask) > 0 ? 
@@ -15,11 +21,13 @@ namespace Application.Services
                 throw new Exception("Falha ao criar tarefa");
         }
 
+        /// <inheritdoc/>
         public async Task<bool> DeleteTaskAsync(int id)
         {
             return await _taskRepository.DeleteTaskAsync(id) > 0;
         }
 
+        /// <inheritdoc/>
         public async Task<IEnumerable<TaskDto>> GetAllTasksAsync(DateTime? date, TaskStatusEnum.Status? taskStatus)
         {
             return await _taskRepository.GetAllTasksAsync(date, taskStatus) is IEnumerable<Task> tasks ?
@@ -27,6 +35,7 @@ namespace Application.Services
                 throw new Exception("Falha ao obter tarefas");
         }
 
+        /// <inheritdoc/>
         public async Task<TaskDto> GetTaskByIdAsync(int id)
         {
             return await _taskRepository.GetTaskByIdAsync(id) is Task task ?
@@ -34,6 +43,7 @@ namespace Application.Services
                 throw new Exception("Tarefa não encontrada");
         }
 
+        /// <inheritdoc/>
         public async Task<bool> UpdateTaskAsync(TaskDto taskDto)
         {
             return await _taskRepository.UpdateTaskAsync(taskDto.ToTask) > 0;
