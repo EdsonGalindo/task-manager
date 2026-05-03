64c7f80e11a3c252c3dae701892c4190d1ba1acf
Edson Galindo <edson@yanel.com.br>
2026-05-03 19:58:37 -0300

    Adiciona extensão para centralizar configurações do Swagger

---
+++ TaskManager/WebApps/TaskManager.WebApp.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager.WebApp.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
